60b8e995e6be45e6470e97af4f35251f5b89b1f5
EllyPirelly <ellypirelly1@yahoo.de>
2021-02-12 14:05:54 +0100

    fix toggle actions, clean-up

--- js/index.js
+++ js/index.js
@@ -1,72 +1,59 @@
+const toggleAction = (toggle, target) => {
+    if(toggle.tagName.toLowerCase() === 'button') {
+        target.classList.toggle('open')
+    } else {
+        target.classList.remove('open')
+    }
+}
+
 const toggleNavBar = () => {
-    const toggles = Array.from(document.querySelectorAll('[data-toggle="nav-toggle"]'))
+    const toggles = Array.from(document.querySelectorAll('[data-toggle]'))
 
     toggles.forEach(toggle => {
         const targetSelector = toggle.getAttribute('data-target')
         const target = document.querySelector(targetSelector)
 
-        toggle.addEventListener('click', () => target.classList.toggle('open'))
+        toggle.addEventListener('click', function() {
+            toggleAction(this, target)
+        })
     })
+}
 
-  }
-
-  const AnchorJump = (function () {
+const AnchorJump = (function () {
     let links
 
     const setLinks = () => {
-      links = Array.from(document.querySelectorAll('[data-link]'))
+        links = Array.from(document.querySelectorAll('[data-link]'))
     }
 
     const linkAction = function (event) {
-      event.preventDefault()
+        event.preventDefault()
 
-      const href = this.getAttribute('href')
-      const target = document.querySelector(href)
+        const href = this.getAttribute('href')
+        const target = document.querySelector(href)
 
-      history.pushState(null, null, href)
-      target.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      })
+        history.pushState(null, null, href)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
     }
 
     const setLinkAction = () => {
-      links.forEach((link) => link.addEventListener('click', linkAction))
+        links.forEach((link) => link.addEventListener('click', linkAction))
     }
 
     const init = () => {
-      setLinks()
-      setLinkAction()
+        setLinks()
+        setLinkAction()
     }
 
     return {
-      init,
+        init,
     }
-  })()
+})()
 
-  document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     AnchorJump.init()
     toggleNavBar()
-  })
-
-  /* code content button */
-  const button = document.querySelector('[data-toggle="content-toggle"]');
-  const content = document.querySelector("#code-content");
-  let showContent = false;
-  const buttonState = {
-    "Click to show more examples" : "Click to hide",
-    "Click to hide" : "Click to show more examples"
-  };
-
-  button.addEventListener("click", () => {
-    showContent = !showContent;
-
-    if (showContent === true) {
-      content.style.display = "grid"
-    } else {
-      content.style.display = "none"
-    };
-
-    button.innerText = buttonState[button.innerText];
-
-  });
+})
