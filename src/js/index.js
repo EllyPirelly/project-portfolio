71deb778da4dc52cd95eeb86d86191832284c31e
EllyPirelly <ellypirelly1@yahoo.de>
2021-03-26 14:43:17 +0100

    add next steps tag filtering

--- src/js/index.js
+++ src/js/index.js
@@ -19,35 +19,104 @@ const toggleVisibility = () => {
   })
 }
 
-/* get button */
-const selectButton = document.getElementById('tagselectbtn')
-const unselectButton = document.getElementById('tagunselectbtn')
+const blogTeaserSelection = (function () {
+  let selectAllButton
+  let unselectAllButton
+  let tagItems
+  let tagLinks
+  let tag
 
-/* add listener */
-selectButton.addEventListener('click', selectAll)
-unselectButton.addEventListener('click', unselectAll)
+  /* array for collected/selected tags */
+  const tagCollection = []
 
-/* todo: refactor functions */
-function selectAll() {
-  console.log('select')
-  let items = document.getElementsByName('tagcheck')
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].type == 'checkbox') {
-      items[i].checked = true
+  /* grab select/unselect button */
+  const getButtonElement = () => {
+    selectAllButton = document.querySelector('[data-button="select"]')
+    unselectAllButton = document.querySelector('[data-button="unselect"]')
+  }
+
+  /* check if select/unselect button element is present, add listener, invoke funcs */
+  const buttonAction = () => {
+    if (selectAllButton !== null && unselectAllButton !== null) {
+      selectAllButton.addEventListener('click', () => {
+        checkAllTagItems()
+        showBlogPosts()
+      })
+      unselectAllButton.addEventListener('click', () => {
+        uncheckAllTagItems()
+        showBlogPosts()
+      })
+    }
+
+    /* grab tagItems (input tag) - check all */
+    function checkAllTagItems() {
+      tagItems = document.querySelectorAll('[data-input="tagcheck"]')
+      for (let i = 0; i < tagItems.length; i++) {
+        if (tagItems[i].type == 'checkbox') {
+          tagItems[i].checked = true
+        }
+      }
+    }
+
+    /* grab tagItems (input tag) - uncheck all */
+    function uncheckAllTagItems() {
+      tagItems = document.querySelectorAll('[data-input="tagcheck"]')
+      for (let i = 0; i < tagItems.length; i++) {
+        if (tagItems[i].type == 'checkbox') {
+          tagItems[i].checked = false
+        }
+      }
+    }
+
+    function showBlogPosts() {
+      // ToDo - show only selected/checked teaser
     }
   }
-}
 
-function unselectAll() {
-  console.log('unselect')
-  let items = document.getElementsByName('tagcheck')
-  for (let i = 0; i < items.length; i++) {
-    console.log('unselect inside for')
-    if (items[i].type == 'checkbox') {
-      items[i].checked = false
+  /* grab tagLinks (a tag) elements */
+  const getTagLinks = () => {
+    tagLinks = Array.from(document.querySelectorAll('[data-tag-link]'))
+  }
+
+  /* add listener on single tagLink element */
+  const tagAction = () => {
+    tagLinks.forEach((tagLink) => {
+      tagLink.addEventListener('click', function () {
+        tag = this.getAttribute('data-tag-link')
+        checkTagLink(tag)
+      })
+    })
+
+    /* tag - push/pull to tagCollection array */
+    function checkTagLink(tag) {
+      if (!tagCollection.includes(tag)) {
+        tagCollection.push(tag)
+      } else if (tagCollection.includes(tag)) {
+        tagCollection.pop(tag)
+      }
+      console.log(tag)
+      console.log(tagCollection)
     }
   }
-}
+
+  /* create URL of selected tags, set created URL back to default */
+  const createURL = () => {
+    // adapt query string to selection -> blogoverview.html?tags=...,...
+    // remove query string from URL back to default/base
+  }
+
+  const init = () => {
+    getButtonElement()
+    buttonAction()
+    getTagLinks()
+    tagAction()
+    createURL()
+  }
+
+  return {
+    init,
+  }
+})()
 
 const AnchorJump = (function () {
   let links
@@ -86,4 +155,5 @@ const AnchorJump = (function () {
 document.addEventListener('DOMContentLoaded', () => {
   AnchorJump.init()
   toggleVisibility()
+  blogTeaserSelection.init()
 })
