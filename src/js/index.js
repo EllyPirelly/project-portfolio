4505aa31ec1d2d586e4b8480e08f33e98df89a15
EllyPirelly <ellypirelly1@yahoo.de>
2021-04-19 11:43:52 +0200

    finalise filter func according to mentoring session

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 // toggle
 const toggleClass = (item, targetItem) => {
     if (item.getAttribute('data-toggle')) {
-        targetItem.classList.toggle('toggle')
+        targetItem.classList.toggle('toggle');
     } else {
-        targetItem.classList.remove('toggle')
+        targetItem.classList.remove('toggle');
     }
 }
 
@@ -20,102 +20,142 @@ const toggleVisibility = () => {
     })
 }
 
-// filter/show blog snippets
-const blogSnippets = (function () {
+// filter/show blog fractions by tags
+const blogFractionSelection = (function () {
+    const blogFractions = [];
+    const defaultTags = [];
+    const selectedTags = [];
 
-    let selectAllButton;
-    let unselectAllButton;
-    let tag;
-    let tagCollection = [];
-    let tagLinks = [];
-    let blogPosts = [];
-
-    const getButtonElement = () => {
-        selectAllButton = document.querySelector('[data-button="select"]');
-        unselectAllButton = document.querySelector('[data-button="unselect"]');
+    const getblogFractions = () => {
+        Array.from(document.querySelectorAll('[data-element="blog-fraction"]')).forEach(blogfraction => blogFractions.push(blogfraction));
     }
 
-    const buttonAction = () => {
-        if (selectAllButton !== null && unselectAllButton !== null) {
-            selectAllButton.addEventListener('click', () => {
-                tagLinkClicked(tagLinks);
-                showBlogPosts(blogPosts);
+    const hideBlogFractions = blogfractions => {
+        blogfractions.forEach(blogfraction => {
+            blogfraction.classList.add('hidden');
+        })
+    }
+
+    const setHiddenBlogFractions = () => {
+        if (selectedTags.length === 0) {
+            return false;
+        }
+
+        const hiddenBlogFractions = blogFractions.filter(blogfraction => {
+            const fractionTags = blogfraction.getAttribute('data-tags').split(' ');
+            return !selectedTags.some(tag => fractionTags.includes(tag));
+        })
+
+        hideBlogFractions(hiddenBlogFractions);
+    }
+
+    const getDefaultTags = () => {
+        blogFractions.forEach(blogfraction => {
+            const tags = blogfraction.getAttribute('data-tags').split(' ');
+
+            tags.forEach(tag => {
+                if (!defaultTags.includes(tag)) {
+                    defaultTags.push(tag);
+                }
             })
-            unselectAllButton.addEventListener('click', () => {
-                tagLinkUnclicked(tagLinks);
-                showBlogPosts(blogPosts);
-            })
+        })
+
+        defaultTags.sort();
+    }
+
+    const getSelectedTags = () => {
+        const queryStringTags = new URLSearchParams(window.location.search);
+
+        if (queryStringTags.has('tags')) {
+            queryStringTags.get('tags').split(',').forEach(tag => selectedTags.push(tag));
         }
     }
 
-    const getTagLinks = () => {
-        tagLinks = Array.from(document.querySelectorAll('[data-tag-link]'));
-    }
+    const getNewUrl = (tags = [], pathName = '/blog/blog-overview.html') => {
+        const queryStrings = new URLSearchParams(window.location.search);
 
-    /* better and cleverer solution to combine both?? */
-    const tagLinkClicked = (tagLinks) => {
-        tagLinks.forEach(tagLink => {
-            tagLink.classList.add('clicked');
-        })
-    }
-
-    const tagLinkUnclicked = (tagLinks) => {
-        tagLinks.forEach(tagLink => {
-            tagLink.classList.remove('clicked');
-        })
-    }
-
-    /* same here, when above can be solved better, same applies to this?? */
-    const tagCollectionPopulation = () => {
-        tagLinks.forEach((tagLink) => {
-            tagLink.addEventListener('click', function () {
-                tag = this.getAttribute('data-tag-link');
-                tagLink.classList.add('clicked');
-                tagLinkPresence(tag);
-            })
-        })
-    }
-
-    function tagLinkPresence(tag) {
-        if (!tagCollection.includes(tag)) {
-            tagCollection.push(tag);
+        if (tags.length > 0) {
+            queryStrings.set('tags', tags.join(','));
         } else {
-            const index = tagCollection.indexOf(tag);
-            tagCollection.splice(index, 1);
+            queryStrings.delete('tags');
         }
-        console.log('tagCollection', tagCollection);
+
+        return `${window.location.origin}${pathName}?${queryStrings.toString()}`;
     }
 
-    const getBlogPosts = () => {
-        blogPosts = Array.from(document.querySelectorAll('[data-element="blog-post"]'));
+    const tagLinkAction = event => {
+        event.preventDefault();
+
+        const tagLink = event.target;
+        const tag = tagLink.getAttribute('data-taglink');
+
+        if (!selectedTags.includes(tag)) {
+            selectedTags.push(tag);
+        } else {
+            selectedTags.splice(selectedTags.indexOf(tag), 1);
+        }
+
+        window.location.href = getNewUrl(selectedTags);
     }
 
-    const showBlogPosts = (posts) => {
-        posts.forEach(post => {
-            post.classList.remove('hide');
+    const setTagFilterLinks = (tagWrapper, tags) => {
+        tags.forEach(tag => {
+            const link = document.createElement('a');
+            link.classList.add('taglink');
+
+            if (selectedTags.includes(tag)) {
+                link.classList.add('selected');
+            }
+
+            link.href = '#';
+            link.setAttribute('data-taglink', tag);
+            link.text = tag;
+            link.addEventListener('click', tagLinkAction);
+
+            tagWrapper.insertAdjacentElement('beforeend', link);
         })
     }
 
-    const createURL = () => {
-        // adapt query string to selection -> /blog/blog-overview.html?tags=...,...
-        // remove query string from URL back to default/base
-        // let baseUrl = window.location.origin
-        // URLSearchParams.append(name, value)
+
+    const buttonSelectAllAction = (selectAll = false) => {
+        if (selectAll) {
+            window.location.href = getNewUrl(defaultTags);
+        } else {
+            window.location.href = getNewUrl();
+        }
+    }
+
+    const setSelectAllButtons = () => {
+        const buttons = Array.from(document.querySelectorAll('[data-button]'));
+
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                const selectAll = button.getAttribute('data-button') === 'select';
+                buttonSelectAllAction(selectAll);
+            })
+        })
     }
 
     const init = () => {
-        getBlogPosts();
-        getButtonElement();
-        getTagLinks();
-        buttonAction();
-        tagCollectionPopulation();
-        createURL();
+        const tagWrapper = document.querySelector('[data-element="tag-filter"]');
+
+        if (tagWrapper === null) return false;
+
+        getblogFractions();
+        getDefaultTags();
+        getSelectedTags();
+
+        setTagFilterLinks(tagWrapper, defaultTags);
+        setHiddenBlogFractions();
+        setSelectAllButtons();
     }
 
     return {
-        init,
+        init
     }
 })()
+
+
 
 /* anchor jump */
 const AnchorJump = (function () {
@@ -156,5 +196,5 @@ const AnchorJump = (function () {
 document.addEventListener('DOMContentLoaded', () => {
     AnchorJump.init();
     toggleVisibility();
-    blogSnippets.init();
+    blogFractionSelection.init();
 })
